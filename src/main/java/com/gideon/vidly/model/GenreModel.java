fa98469b156b1eb4ec3ee277514fbdb2b19a78cf
GideonAg <gideonagbosu72@gmail.com>
2023-05-08 14:53:05 +0000

    Added GenreModel

---
+++ src/main/java/com/gideon/vidly/model/GenreModel.java
@@ -0,0 +1,12 @@
+package com.gideon.vidly.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenreModel {
+    private String title;
+}
