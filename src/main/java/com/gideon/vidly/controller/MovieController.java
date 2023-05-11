f7265162631afb478354be052ecfc6e3868ba63f
GideonAg <gideonagbosu72@gmail.com>
2023-05-11 22:32:29 +0000

    Modified http request of update movie endpoint
    Change request type from PostMapping to PutMapping

--- src/main/java/com/gideon/vidly/controller/MovieController.java
+++ src/main/java/com/gideon/vidly/controller/MovieController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,7 @@ public class MovieController {
         return response;
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public String updateMovie(@PathVariable("id") Long id, @RequestBody MovieModel movieModel) {
         String response = movieService.updateMovie(id, movieModel);
         return response;
